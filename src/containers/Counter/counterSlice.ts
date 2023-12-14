66eda8b6b8a1ff6c2140de3346a614e0fd1f76cd
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-14 14:52:14 +0600

    Added counterSlice.ts

---
+++ src/containers/Counter/counterSlice.ts
@@ -0,0 +1,29 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface CounterState {
+    value: string;
+}
+
+const initialState: CounterState = {
+    value: '',
+};
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        count: (state) => {
+            state.value = eval(state.value);
+            console.log(state.value);
+
+        },
+        show: (state,action: PayloadAction<string>) => {
+            state.value += action.payload;
+        },
+    }
+});
+
+export const counterReducer = counterSlice.reducer;
+export const {
+    count,
+    show,
+} = counterSlice.actions;
