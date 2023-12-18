e38ba6eb820ef21552343b860e409fa5b20f1530
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-18 17:26:26 +0600

    Added style Counter.tsx

--- src/containers/Counter/Counter.tsx
+++ src/containers/Counter/Counter.tsx
@@ -6,28 +6,31 @@ const Counter = () => {
     const counterValue = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch();
     return (
-        <div className="Counter">
-            <div className="bg-light">{counterValue}</div>
-            <div className="m-2">
+        <div className="Counter border-black-3">
+            <div className="mb-1">
+                <div className="bg-light">{counterValue}</div>
+            </div>
+            <div className="mb-1">
                 <button onClick={() => dispatch(show('+'))}>+</button>&nbsp;
                 <button onClick={() => dispatch(show('-'))}>-</button>&nbsp;
                 <button onClick={() => dispatch(show('*'))}>*</button>&nbsp;
-                <button onClick={() => dispatch(show('/'))}>/</button>
+                <button onClick={() => dispatch(show('/'))}>/</button>&nbsp;
             </div>
-            <div className='m-2'>
+            <div className='mb-1'>
                 <button onClick={()=> dispatch(show('1'))}>1</button>&nbsp;
                 <button onClick={()=> dispatch(show('2'))}>2</button>&nbsp;
                 <button onClick={()=> dispatch(show('3'))}>3</button>&nbsp;
-                <button onClick={()=> dispatch(show('4'))}>4</button>
+                <button onClick={()=> dispatch(show('4'))}>4</button>&nbsp;
             </div>
-            <div className="m-2">
+            <div className="mb-1">
                 <button onClick={()=> dispatch(show('5'))}>5</button>&nbsp;
                 <button onClick={()=> dispatch(show('6'))}>6</button>&nbsp;
                 <button onClick={()=> dispatch(show('7'))}>7</button>&nbsp;
                 <button onClick={()=> dispatch(show('8'))}>8</button>&nbsp;
             </div>
-            <div className='m-2'>
+            <div className='mb-1'>
                 <button onClick={()=> dispatch(show('9'))}>9</button>&nbsp;
+                <button onClick={()=> dispatch(show('0'))}>0</button>&nbsp;
                 <button onClick={() => dispatch(count())}>=</button>
             </div>
         </div>
